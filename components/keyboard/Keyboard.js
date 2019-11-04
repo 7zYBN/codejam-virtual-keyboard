eb74132b852b934780fa258c02ee9db6191168de
7zYBN <frazo4ka@gmail.com>
2019-11-04 17:00:17 +0300

    feat: implement keyboard building

--- components/keyboard/Keyboard.js
+++ components/keyboard/Keyboard.js
@@ -1,3 +1,5 @@
+import Button from "../button/Button.js"
+
 export default class Keyboard {
   constructor(parent, template) {
     this._parent = parent;
@@ -6,6 +8,9 @@ export default class Keyboard {
     this._possibleLanguages = this._getPossibleLanguagesFromTemplate();
     this._language = this._setLanguageFromTemplate();
     this._upperCase = false;
+    this._wrapper = this._buildWrapper();
+    
+    this._buildKeyboard();
   }
 
   _getPossibleLanguagesFromTemplate() {
@@ -23,5 +28,42 @@ export default class Keyboard {
     if (!this._possibleLanguages) throw error;
 
     return this._possibleLanguages.filter(language => !language.includes('upper'))[firstLanguageIndex];
+  }
+
+  _buildWrapper() {
+    const wrapper = document.createElement('div');
+
+    wrapper.classList.add('keyboard-wrapper');
+    this._parent.appendChild(wrapper);
+
+    return  wrapper;
+  }
+
+  _buildKeyboard() {
+    this._template.forEach((row, rowIndex) => {
+      const keyboardRow = this._createKeyboardRow(rowIndex);
+
+      row.forEach(button => {
+        this._initButton(button, keyboardRow);
+      })
+
+    })
+  }
+
+  _createKeyboardRow(rowIndex) {
+    const row = document.createElement('div');
+
+    row.classList.add(`keyboard-row-${rowIndex}`);
+    this._wrapper.appendChild(row);
+
+    return row;
+  }
+
+  _initButton(button, parent) {
+    const key = Object.keys(button)[0];
+    const values = Object.values(button);
+    const value = this._upperCase ? [`${this._language}-upper`] : [this._language];
+
+    new Button(values[0][value], key, parent);
   }
 }
